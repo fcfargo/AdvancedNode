233606042741e65316e88859552b0de05fbe72d7
fcfargo <fcfargo90@gmail.com>
2023-01-27 19:20:45 +0900

    update React App: add image picker && file changes handler

--- AdvancedNodeStarter/client/src/actions/index.js
+++ AdvancedNodeStarter/client/src/actions/index.js
@@ -1,32 +1,36 @@
 import axios from 'axios';
 import { FETCH_USER, FETCH_BLOGS, FETCH_BLOG } from './types';
 
-export const fetchUser = () => async dispatch => {
+export const fetchUser = () => async (dispatch) => {
   const res = await axios.get('/api/current_user');
 
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const handleToken = token => async dispatch => {
+export const handleToken = (token) => async (dispatch) => {
   const res = await axios.post('/api/stripe', token);
 
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const submitBlog = (values, history) => async dispatch => {
+export const submitBlog = (values, history) => async (dispatch) => {
+  // api 통신을 통해 블로그 포스트 데이터 생성
   const res = await axios.post('/api/blogs', values);
 
+  // 유저를 특정 /blogs 페이지로 리다이렉트
   history.push('/blogs');
+
+  // 새롭게 생성된 데이터를 브라우저 화면에 rendering하기 위해 Redux side에 블로그 포스트 데이터가 생성됐음을 알림
   dispatch({ type: FETCH_BLOG, payload: res.data });
 };
 
-export const fetchBlogs = () => async dispatch => {
+export const fetchBlogs = () => async (dispatch) => {
   const res = await axios.get('/api/blogs');
 
   dispatch({ type: FETCH_BLOGS, payload: res.data });
 };
 
-export const fetchBlog = id => async dispatch => {
+export const fetchBlog = (id) => async (dispatch) => {
   const res = await axios.get(`/api/blogs/${id}`);
 
   dispatch({ type: FETCH_BLOG, payload: res.data });
--- AdvancedNodeStarter/client/src/components/blogs/BlogFormReview.js
+++ AdvancedNodeStarter/client/src/components/blogs/BlogFormReview.js
@@ -7,6 +7,8 @@ import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
 class BlogFormReview extends Component {
+  state = { file: null };
+
   renderFields() {
     const { formValues } = this.props;
 
@@ -25,10 +27,7 @@ class BlogFormReview extends Component {
 
     return (
       <div>
-        <button
-          className="yellow darken-3 white-text btn-flat"
-          onClick={onCancel}
-        >
+        <button className="yellow darken-3 white-text btn-flat" onClick={onCancel}>
           Back
         </button>
         <button className="green btn-flat right white-text">
@@ -39,12 +38,16 @@ class BlogFormReview extends Component {
     );
   }
 
+  onFileChange(event) {
+    this.setState({ file: event.target.files[0] });
+  }
+
   onSubmit(event) {
     event.preventDefault();
 
     const { submitBlog, history, formValues } = this.props;
 
-    submitBlog(formValues, history);
+    submitBlog(formValues, this.state.file, history);
   }
 
   render() {
@@ -52,6 +55,9 @@ class BlogFormReview extends Component {
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
+
+        <h5>Add An Image</h5>
+        <input onChange={this.onFileChange.bind(this)} type="file" accept="image/*" />
 
         {this.renderButtons()}
       </form>
